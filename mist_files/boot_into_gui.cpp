7597bb83b67bd48169212ce0af7fb801bd62df2c
RedXer0 <redxer0@proton.me>
2024-04-18 11:06:59 -0500

    modified:   mist_files/boot_into_gui.cpp

--- mist_files/boot_into_gui.cpp
+++ mist_files/boot_into_gui.cpp
@@ -4,7 +4,7 @@
 int main(){
     printf("\033[32m booting into gui...");
     sleep(2);
-    system("sudo systemctl set-default graphical.target");
+    system("sudo systemctl set-defalt graphical.target");
     sleep(2);
     printf("rebooting...");
     sleep(2);
